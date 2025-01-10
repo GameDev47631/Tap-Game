97c8acf5578805535e7aa19bd1a57d1ba03bda0a
Jonathan Augustin <jonaug756@gmail.com>
2025-01-10 18:26:41 -0500

    Some scripts had unused headers.

--- Assets/Scripts/KillBat.cs
+++ Assets/Scripts/KillBat.cs
@@ -1,7 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class KillBat : MonoBehaviour {
     [SerializeField] GameObject DeathEffect;
